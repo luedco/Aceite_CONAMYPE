14783060abaf62db5d824f60d5a051a4b90e8755
luedco <luedcomu@gmail.com>
2021-06-10 16:23:10 -0600

    agregar imagenes de prueba

--- client/src/components/mainpages/mainPage/MainPage.js
+++ client/src/components/mainpages/mainPage/MainPage.js
@@ -2,7 +2,8 @@ import React, { useContext, useEffect } from 'react'
 import { GlobalState } from '../../../GlobalState'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-
+import img2 from './img2.gif';
+import './mainPage.css'
 
 function MainPage() {
     return (
@@ -11,7 +12,8 @@ function MainPage() {
                 PRUEBA DE MainPage
             </h2>
             <p>
-                Somos un emprendimiento de aceites.
+                <img src={img2} alt="gg" />
+                <div class="separator">Next section</div>
             </p>
         </div>
     )
