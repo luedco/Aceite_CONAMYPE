0010402d265bd89297ce783e26107b67fdd13e5c
Daniel Hernandez <00209317@uca.edu.sv>
2021-06-11 01:10:57 -0600

    cards

--- client/src/components/mainpages/mainPage/MainPage.js
+++ client/src/components/mainpages/mainPage/MainPage.js
@@ -2,18 +2,25 @@ import React, { useContext, useEffect } from 'react'
 import { GlobalState } from '../../../GlobalState'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import img2 from './img2.gif';
 import img1 from './discount.jpg'
-import img3 from './img3.jpeg'
-import './mainPage.css'
+import img2 from './img2.gif';
+import img3 from './img3.jpeg';
+import ml10 from './10ml.jpg';
+import ml20 from './20ml.jpg';
+import ml30 from './30ml.jpg';
+import './mainPage.css';
+import './social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button';
 
 function MainPage() {
     return (
         <div className="qSomos-page">
             <h2>
-                PRUEBA DE MainPage
+
             </h2>
             <Carousel>
                 <Carousel.Item interval={1000}>
@@ -50,10 +57,60 @@ function MainPage() {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
+            <br></br>
             <p>
-                <img src={img2} alt="gg" />
-                <div class="separator">Next section</div>
+                <div class="separator"></div>
             </p>
+            <br></br>
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src={ml10} />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={ml20} />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={ml30} />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+
+            <div class="footer"></div>
+
+
+
+
+
         </div>
     )
 }
