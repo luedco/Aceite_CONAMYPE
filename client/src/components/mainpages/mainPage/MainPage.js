d7fcc95bbd212f8307e419e0b3ab26e502a48da9
Daniel Hernandez <00209317@uca.edu.sv>
2021-06-10 22:53:16 -0600

    carrusel añadido

--- client/src/components/mainpages/mainPage/MainPage.js
+++ client/src/components/mainpages/mainPage/MainPage.js
@@ -3,7 +3,11 @@ import { GlobalState } from '../../../GlobalState'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import img2 from './img2.gif';
+import img1 from './discount.jpg'
+import img3 from './img3.jpeg'
 import './mainPage.css'
+import Example from './Carrusel'
+import Carousel from 'react-bootstrap/Carousel'
 
 function MainPage() {
     return (
@@ -11,6 +15,41 @@ function MainPage() {
             <h2>
                 PRUEBA DE MainPage
             </h2>
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={img1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img3}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
             <p>
                 <img src={img2} alt="gg" />
                 <div class="separator">Next section</div>
