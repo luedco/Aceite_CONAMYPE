69f1f92068e20108295b599e8aa73d86cabb36d2
Daniel Hernandez <00209317@uca.edu.sv>
2021-07-16 02:11:30 -0600

    mejoras en fundadores y contactanos

--- client/src/components/mainpages/contacto/Contacto.js
+++ client/src/components/mainpages/contacto/Contacto.js
@@ -2,17 +2,110 @@ import React, { useContext, useEffect } from 'react'
 import { GlobalState } from '../../../GlobalState'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Footer from '../footer/Footer'
 
 
 function Contacto() {
     return (
         <div className="qSomos-page">
-            <h2>
-                PRUEBA DE Contacto
-            </h2>
-            <p>
-                Somos un emprendimiento de aceites.
-            </p>
+            <br></br>
+            <Container fluid="md">
+                {/* Stack the columns on mobile by making one full-width and the other half-width */}
+                <Row className="justify-content-md-center">
+                    <Col md="auto" >
+                        <Container>
+                            <h3>
+                                <Badge bg="secondary">Llámanos</Badge>
+                            </h3></Container>
+                        <Container>
+                            <h5>
+                                <Badge bg="secondary">Oficinas Centrales Sonsonate: 2240-0046</Badge>
+                            </h5></Container>
+                        <Container>
+                            <h5>
+                                <Badge bg="secondary">Oficinas Secundarias: 2240-0047</Badge>
+                            </h5></Container>
+
+                    </Col>
+                    <Col md="auto">
+                        <Container><h3>
+                            <Badge bg="secondary">Horario:</Badge>
+                        </h3></Container>
+                        <Container><h5>
+                            <Badge bg="secondary">Horarios de lunes a viernes:</Badge>
+                        </h5></Container>
+                        <Container><h5>
+                            <Badge bg="secondary">8:00 a.m - 5:00 p.m</Badge>
+                        </h5></Container>
+                        <Container><h5>
+                            <Badge bg="secondary">Horarios de sabado:</Badge>
+                        </h5></Container>
+                        <Container><h5>
+                            <Badge bg="secondary">8:00 a.m - 12:00 p.m</Badge>
+                        </h5></Container>
+                    </Col>
+                    <Col md="auto">
+                        <Container><Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label><Badge bg="secondary">Nombre:</Badge></Form.Label>
+                                <Form.Control type="text" placeholder="Ingresa tu nombre" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label><Badge bg="secondary">Email:</Badge></Form.Label>
+                                <Form.Control type="email" placeholder="nombre@ejemplo.com" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label><Badge bg="secondary">Comentario:</Badge></Form.Label>
+                                <Form.Control as="textarea" rows={2} />
+                                <br></br>
+                                <Button variant="primary" type="submit">
+                                    Enviar
+                                </Button>
+                            </Form.Group>
+                        </Form></Container>
+                    </Col>
+                </Row>
+
+                <Row className="justify-content-md-center">
+                    <Col xs={6}>
+                        <Container>
+                            <h3>
+                                <Badge bg="secondary">Ubicación Oficinas Centrales Sonsonate:</Badge>
+                            </h3></Container>
+                        <Container>
+                            <h5>
+                                <Badge bg="secondary"> Departamento de Sonsonate, colonia El Balsamar, pasaje 2</Badge>
+                            </h5></Container>
+                    </Col>
+                    <Col xs={6}>
+                        <Container><h3>
+                            <Badge bg="secondary">Ubicación Oficinas Secundarias:</Badge>
+                        </h3></Container>
+                        <Container><h5>
+                            <Badge bg="secondary">Colonia Roma 67 Av. Sur 13-B, San Salvador, El Salvador</Badge>
+                        </h5></Container>
+                    </Col>
+                </Row>
+
+                {/* Columns are always 50% wide, on mobile and desktop */}
+                <Row className="justify-content-md">
+                    <Col xs={6}>
+                        <Container><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9494.657707429744!2d-89.71509361178546!3d13.716381546707085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f62b7edf2822a2f%3A0x23e8211df1c6a9a5!2sPasaje%202%2C%20Sonsonate!5e0!3m2!1ses!2ssv!4v1626402655943!5m2!1ses!2ssv" width="100%" height="225px" allowfullscreen="" loading="lazy"></iframe></Container>
+                    </Col>
+                    <Col xs={6}>
+                        <Container><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3876.2954369632007!2d-89.22672078534634!3d13.700548190380651!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f6330413ca1b54b%3A0x25fe4f3a30547156!2s67%20Avenida%20Sur%2013%2C%20San%20Salvador!5e0!3m2!1ses!2ssv!4v1626415805189!5m2!1ses!2ssv" width="100%" height="225px" allowfullscreen="" loading="lazy"></iframe></Container>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer></Footer>
         </div>
     )
 }
--- client/src/components/mainpages/quienesSomos/fundadores.js
+++ client/src/components/mainpages/quienesSomos/fundadores.js
@@ -18,169 +18,169 @@ let imgs = [
     'https://res.cloudinary.com/dwwgiwtgh/image/upload/v1624248410/fundadores/Mario_qbh4ru.jpg'
 ];
 
-function fundadores(){
-    return(
+function fundadores() {
+    return (
         <div className="containerFundadores" id="fundadoresId">
-                <div class="separator"><h2>Fundadores</h2></div>
-                <CardDeck className="rowE">
-                    <Row className="colE">
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[0]} className="cardImg" />
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Ing. Grace Flores</Card.Title>
-                                        <Card.Text>
-                                            Me gustó mucho que pude aplicar los conocimientos sobre mi carrera y adquirir nuevos sobre otros rubros. Además, conocí muy buena gente.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Col>
-                    </Row>
-                    <Row>
+            <div class="separator"><h2>Fundadores</h2></div>
+            <CardDeck>
+                <Card>
+                    <Card.Img variant="top" src={imgs[0]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto'}}></Card.Img>
+                    <Card.Body>
+                        <Card.Title>Ing. Grace Flores</Card.Title>
+                        <Card.Text>
+                            Me gustó mucho que pude aplicar los conocimientos sobre mi carrera y adquirir nuevos sobre otros rubros. Además, conocí muy buena gente.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted"></small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={imgs[1]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                    <Card.Body>
+                        <Card.Title>Ing. Sugeidi Pérez</Card.Title>
+                        <Card.Text>
+                            La parte que más me ha gustado del emprendimiento es que he podido poner a prueba mis habilidades y lo aprendido hasta el día de hoy, aprendiendo siempre también de los demás y del trabajo en equipo.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted"></small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck>
+            <br></br>
+            <CardDeck className="rowE">
+                <Row className="colE">
                     <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[1]} className="cardImg" />
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Ing. Sugeidi Pérez</Card.Title>
-                                        <Card.Text>
-                                            La parte que más me ha gustado del emprendimiento es que he podido poner a prueba mis habilidades y lo aprendido hasta el día de hoy, aprendiendo siempre también de los demás y del trabajo en equipo.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[0]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                            <Card.Body>
+                                    <Card.Title>Ing. Grace Flores</Card.Title>
+                                    <Card.Text>
+                                        Me gustó mucho que pude aplicar los conocimientos sobre mi carrera y adquirir nuevos sobre otros rubros. Además, conocí muy buena gente.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[1]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                            <Card.Body>
+                                    <Card.Title>Ing. Sugeidi Pérez</Card.Title>
+                                    <Card.Text>
+                                        La parte que más me ha gustado del emprendimiento es que he podido poner a prueba mis habilidades y lo aprendido hasta el día de hoy, aprendiendo siempre también de los demás y del trabajo en equipo.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
 
-                        </Col>
-                    </Row>
-                    <Row className="colE">
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[2]} className="cardImg" />
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Ing. Clery Giron</Card.Title>
-                                        <Card.Text>
-                                            El emprendimiento me ha gustado porque me ayuda a superarme a través de mis conocimientos y todo lo que puedo aportar para sacarlo adelante y se que entre todos nos hemos llegado a visualizar hasta dónde podemos llegar si trabajamos juntos.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[3]} className="cardImg" />
-                                </Card>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Ing. Gabriela Cierra</Card.Title>
-                                        <Card.Text>
-                                            Lo que más me ha gustado es poder poner en práctica todo lo aprendido durante la carrera y saber que esto que estamos haciendo al final les pueda servir a  otras personas.                                         </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row className="colE">
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[4]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                                <Card.Body>
+                                    <Card.Title>Ing. Luis Cortez</Card.Title>
+                                    <Card.Text>
+                                        Me gustó mucho que pude aplicar los conocimientos sobre mi carrera y adquirir nuevos sobre otros rubros. Además, conocí muy buena gente.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[5]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                                <Card.Body>
+                                    <Card.Title>Ing. Daniel Hernández</Card.Title>
+                                    <Card.Text>
+                                        La parte que más me ha gustado del emprendimiento es que he podido poner a prueba mis habilidades y lo aprendido hasta el día de hoy, aprendiendo siempre también de los demás y del trabajo en equipo.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
 
-                        </Col>
-                    </Row>
-                    <Row className="colE">
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[4]} className="cardImg" />
-                                </Card>
-                                <Card className="cardFe">
-                                    <Card.Body>
-                                        <Card.Title>Ing. Luis Cortez</Card.Title>
-                                        <Card.Text>
-                                            Me gustó mucho que pude aplicar los conocimientos sobre mi carrera y adquirir nuevos sobre otros rubros. Además, conocí muy buena gente.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[5]} className="cardImg" />
-                                </Card>
-                                <Card className="cardFe">
-                                    <Card.Body>
-                                        <Card.Title>Ing. Daniel Hernández</Card.Title>
-                                        <Card.Text>
-                                            La parte que más me ha gustado del emprendimiento es que he podido poner a prueba mis habilidades y lo aprendido hasta el día de hoy, aprendiendo siempre también de los demás y del trabajo en equipo.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row className="colE">
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[6]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                                <Card.Body>
+                                    <Card.Title>Lic. Marielos Najarro</Card.Title>
+                                    <Card.Text>
+                                        El proyecto resultó ser una puerta de aprendizaje y de retroalimentación, de los aspectos que ya conozco de mi carrera. Asimismo, me gusto compartir mis conocimientos con otras áreas que se complementan, con el fin de concretar y crear un emprendimiento exitoso.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[7]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                                <Card.Body>
+                                    <Card.Title>Lic. Ivannia Urquilla</Card.Title>
+                                    <Card.Text>
+                                        En el desarrollo del emprendimiento me ha gustado llevar a la práctica todos los conocimientos adquiridos a lo largo de la carrera, y el adquirir conocimientos de otras áreas como lo son la ingeniería. Siempre soy del pensamiento que nunca dejamos de aprender.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                        <br></br>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row className="colE">
+                    <Col>
+                        <Row>
+                            <Card className="cardF">
+                                <Card.Img variant="top" src={imgs[8]} style={{ width: '15em', height: '15em', display: 'block', marginLeft: 'auto', marginRight: 'auto', marginBottom:'auto', marginTop:'auto' }} />
+                            </Card>
+                            <Card className="cardFe">
+                                <Card.Body>
+                                    <Card.Title>Ing. Mario Martinez</Card.Title>
+                                    <Card.Text>
+                                        Me resultó un proyecto interesante ya que me permitió aplicar las diferentes herramientas que gracias al estudio de mi carrera he ido aprendiendo.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Card className="cardF"> </Card>
+                            <Card className="cardFe">
+                                
+                            </Card>
+                        </Row>
+                    </Col>
+                </Row>
+                <br></br><br></br>
 
-                        </Col>
-                    </Row>
-                    <Row className="colE">
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[6]} className="cardImg" />
-                                </Card>
-                                <Card className="cardFe">
-                                    <Card.Body>
-                                        <Card.Title>Lic. Marielos Najarro</Card.Title>
-                                        <Card.Text>
-                                            El proyecto resultó ser una puerta de aprendizaje y de retroalimentación, de los aspectos que ya conozco de mi carrera. Asimismo, me gusto compartir mis conocimientos con otras áreas que se complementan, con el fin de concretar y crear un emprendimiento exitoso.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Col>
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[7]} className="cardImg" />
-                                </Card>
-                                <Card className="cardFe">
-                                    <Card.Body>
-                                        <Card.Title>Lic. Ivannia Urquilla</Card.Title>
-                                        <Card.Text>
-                                            En el desarrollo del emprendimiento me ha gustado llevar a la práctica todos los conocimientos adquiridos a lo largo de la carrera, y el adquirir conocimientos de otras áreas como lo son la ingeniería. Siempre soy del pensamiento que nunca dejamos de aprender.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-
-                        </Col>
-                    </Row>
-                    <Row className="colE">
-                        <Col>
-                            <Row>
-                                <Card className="cardF">
-                                    <Card.Img variant="top" src={imgs[8]} className="cardImg" />
-                                </Card>
-                                <Card className="cardFe">
-                                    <Card.Body>
-                                        <Card.Title>Ing. Mario Martinez</Card.Title>
-                                        <Card.Text>
-                                            Me resultó un proyecto interesante ya que me permitió aplicar las diferentes herramientas que gracias al estudio de mi carrera he ido aprendiendo.
-                                        </Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Row>
-                        </Col>
-                        <Col>
-
-
-                        </Col>
-                    </Row>
-                    <br></br><br></br>
-
-                </CardDeck>
-            </div>
+            </CardDeck>
+        </div>
     )
 }
 
